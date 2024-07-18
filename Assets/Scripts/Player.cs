9cfb515be69a57f8821b6bb0927c8459cf4ee7ee
yeoreum <jeondaegam@naver.com>
2024-07-18 14:46:08 +0900

    feat: Player관련 스크립트, 프리팹 추가

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string Name;
+    public int Health;
+    public int Energy;
+    public int Money;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public string Name;
+    public int Health;
+    public int Energy;
+    public int Money;
+}
